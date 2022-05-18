4e3ddb79205e28d1f9ce73eb67e0557fc037686d
shaojiemike <943648187@qq.com>
2022-05-18 21:17:09 +0800

    step5 correct Answer

--- src/5-mallocAndCheck.cu
+++ src/5-mallocAndCheck.cu
@@ -11,14 +11,17 @@ using std::chrono::milliseconds;
 using std::chrono::seconds;
 using std::chrono::system_clock;
 
+#define ProblemSize 14
 #define TPBX 32
 #define TPBY 32
 #define RAD 1 // radius of the stencil
 
+__device__ bool isResultRight = true;
 
 __global__ void stencil(int row_num, int col_num, int *arr_data, int *result) {
-    
-    if (blockIdx.x==0||blockIdx.x==511||blockIdx.y==0||blockIdx.y==511){
+    auto xborderNum=(1 << ProblemSize)/TPBX-1;
+    auto yborderNum=(1 << ProblemSize)/TPBY-1;
+    if (blockIdx.x==0||blockIdx.x==xborderNum||blockIdx.y==0||blockIdx.y==yborderNum){
         auto global_row = blockIdx.x * blockDim.x + threadIdx.x;
         auto global_col = blockIdx.y * blockDim.y + threadIdx.y;
         auto index = global_row * col_num + global_col;
@@ -33,7 +36,7 @@ __global__ void stencil(int row_num, int col_num, int *arr_data, int *result) {
         // right
         auto data4 = arr_data[global_row * col_num + (global_col + 1) % col_num];
 
-        result[index] = data1 + data2 + data3 + data4 - 4 * data0;
+        result[index] = data1 + data2 + data3 + data4 - 7 * data0;
     }else{
         auto global_row = blockIdx.x * blockDim.x + threadIdx.x; //hyq写反了，我也懒得改了记得x是行号就行
         auto global_col = blockIdx.y * blockDim.y + threadIdx.y;
@@ -45,7 +48,7 @@ __global__ void stencil(int row_num, int col_num, int *arr_data, int *result) {
         auto local_row = threadIdx.x + RAD;
         auto local_col = threadIdx.y + RAD;
         auto local_col_num = TPBY + 2 * RAD;
-        auto block_size = TPBX * TPBY;
+        auto block_size = TPBX * local_col_num;
         auto local_index = local_row * local_col_num + local_col;
 
         // Regular cells
@@ -62,8 +65,11 @@ __global__ void stencil(int row_num, int col_num, int *arr_data, int *result) {
         }
         __syncthreads();
 
-        result[index] = sdata[local_index-local_col_num] + sdata[local_index+local_col_num] 
-                    + sdata[local_index-1] + sdata[local_index+1] - 7 * sdata[local_index];
+        result[index] = sdata[local_index - local_col_num]
+                    +   sdata[local_index + local_col_num] 
+                    +   sdata[local_index - 1]
+                    +   sdata[local_index + 1] 
+                - 7 *   sdata[local_index];
     }
 }
 
@@ -74,22 +80,32 @@ __global__ void initArr(int row_num, int col_num, int *arr_data){
     arr_data[index] = 1; 
 }
 
-__global__ void checkResultr(int row_num, int col_num, int *result_data){
+__global__ void checkResult(int row_num, int col_num, int *result_data){
     auto globalx = blockIdx.x * blockDim.x + threadIdx.x;
     auto globaly = blockIdx.y * blockDim.y + threadIdx.y;
     auto index = globaly * col_num + globalx;
     if(result_data[index]!=-3){
-        if(isResultRight = true){
+        if(isResultRight == true){
             isResultRight = false;
         }
     }
 }
 
-__device__ bool isResultRight = true;
+__host__ void debugPrintCudaMatrix(int row_num, int col_num, int *inputCuda){
+    int *tmpPrint = (int *)malloc(sizeof(int) * row_num * col_num);
+    if(cudaSuccess != cudaMemcpy(tmpPrint , inputCuda, sizeof(int) * row_num * col_num, cudaMemcpyDeviceToHost)) 
+        printf("cudaMemcpy Wrong in %s",__func__);
+    for(int i=0; i<row_num; i++){
+        for(int j=0; j<col_num; j++){
+            printf("%d ",tmpPrint[i*col_num+j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    int row_num = 1 << 14;
-    int col_num = 1 << 14;
+    int row_num = 1 << ProblemSize;
+    int col_num = 1 << ProblemSize;
 
     int *arr;
     int *result;
@@ -99,6 +115,8 @@ int main() {
     initArr<<<dim3(row_num / TPBX, col_num / TPBY, 1), dim3(TPBX, TPBY, 1)>>>(row_num, col_num, arr);
 
     cudaDeviceSynchronize();
+    if(ProblemSize <= 6)
+        debugPrintCudaMatrix(row_num, col_num, arr);
     auto begin_millis = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 
     cudaDeviceSynchronize();
@@ -110,8 +128,13 @@ int main() {
     printf("%ld\n", end_millis - begin_millis);
 
     checkResult<<<dim3(row_num / TPBX, col_num / TPBY, 1), dim3(TPBX, TPBY, 1)>>>(row_num, col_num, result);
-
-    if(isResultRight){
+    cudaDeviceSynchronize(); 
+    if(ProblemSize <= 6)
+        debugPrintCudaMatrix(row_num, col_num, result);
+    bool isResultRightHost;
+    // cudaMemcpy(&isResultRightHost, &isResultRight, sizeof(isResultRightHost), cudaMemcpyDeviceToHost);
+    cudaMemcpyFromSymbol(&isResultRightHost, isResultRight, sizeof(bool), 0, cudaMemcpyDeviceToHost);
+    if(isResultRightHost){
         printf("Right Answer!!!\n");
     }else{
         printf("Ops!!! wrong Answer~\n");
