f8b0ee5ac428a8d407c2952acf4794a596fd8421
shaojiemike <943648187@qq.com>
2022-05-22 23:14:35 +0800

    fix shell script

--- compareAvgTime.sh
+++ compareAvgTime.sh
@@ -6,6 +6,8 @@ tryTime=2
 shPath=`pwd`
 fileDir="./build/bin"
 
+# filterNeed="."
+filterNeed="$fileDir/((1|2|3|5|6|7).*)|(45P.*)"
 judgeFileNameSuffix=".*flags"
 isBaseline0=1
 isBaseline1=1
@@ -21,46 +23,48 @@ rm -rf totalAcc1.log
 
 for eachfile in "$fileDir"/* 
 do
-   echo "Filename: $eachfile"
-   tmpSum=0
-   for ((j = 0 ; j < $tryTime ; j++)); do
-        tmp=`$eachfile | awk '{if($1 ~ /^[0-9]+$/) print $1;}'`
-        let "tmpSum += tmp"
-        echo "  tryTime: $j: $tmp $tmpSum"
-   done
-   # let "avg = tmpSum/tryTime" # bash only int
-   avg=`echo "scale=2; $tmpSum/$tryTime" | bc`
-   echo "   avgTime: $avg"
-   # https://stackoverflow.com/questions/18709962/regex-matching-in-a-bash-if-statement
-   if [[ $eachfile =~ $judgeFileNameSuffix ]]; then
-      echo "   match Suffix!"
-      if [[ $isBaseline0 == 1 ]]; then
-         echo "   write baseline 0"
-         baseline0=$avg
+   if [[ $eachfile =~ $filterNeed ]]; then
+      echo "Filename: $eachfile"
+      tmpSum=0
+      for ((j = 0 ; j < $tryTime ; j++)); do
+         tmp=`$eachfile | awk '{if($1 ~ /^[0-9]+$/) print $1;}'`
+         let "tmpSum += tmp"
+         echo "  tryTime: $j: $tmp $tmpSum"
+      done
+      # let "avg = tmpSum/tryTime" # bash only int
+      avg=`echo "scale=2; $tmpSum/$tryTime" | bc`
+      echo "   avgTime: $avg"
+      # https://stackoverflow.com/questions/18709962/regex-matching-in-a-bash-if-statement
+      if [[ $eachfile =~ $judgeFileNameSuffix ]]; then
+         # echo "   match Suffix!"
+         if [[ $isBaseline0 == 1 ]]; then
+            echo "   write baseline 0"
+            baseline0=$avg
+            beforeTime0=$avg
+            isBaseline0=0       
+         fi
+         echo "${avg}" >> time0.log
+         stepAcc=`echo "scale=2; $beforeTime0/$avg" | bc`
          beforeTime0=$avg
-         isBaseline0=0       
-      fi
-      echo "${avg}" >> time0.log
-      stepAcc=`echo "scale=2; $beforeTime0/$avg" | bc`
-      beforeTime1=$avg
-      totalAcc=`echo "scale=2; $baseline0/$avg" | bc`
-      echo "   stepAcc: $stepAcc, totalAcc: $totalAcc"
-      echo "$stepAcc" >> stepAcc0.log
-      echo "$totalAcc" >> totalAcc0.log
-   else
-      echo "   Not match Suffix!"
-      if [[ $isBaseline1 == 1 ]]; then
-         echo "   write baseline 1"
-         baseline1=$avg
+         totalAcc=`echo "scale=2; $baseline0/$avg" | bc`
+         echo "   stepAcc: $stepAcc, totalAcc: $totalAcc"
+         echo "$stepAcc" >> stepAcc0.log
+         echo "$totalAcc" >> totalAcc0.log
+      else
+         # echo "   Not match Suffix!"
+         if [[ $isBaseline1 == 1 ]]; then
+            echo "   write baseline 1"
+            baseline1=$avg
+            beforeTime1=$avg
+            isBaseline1=0
+         fi
+         echo "${avg}" >> time1.log
+         stepAcc=`echo "scale=2; $beforeTime1/$avg" | bc`
          beforeTime1=$avg
-         isBaseline1=0
+         totalAcc=`echo "scale=2; $baseline1/$avg" | bc`
+         echo "   stepAcc: $stepAcc, totalAcc: $totalAcc"
+         echo "$stepAcc" >> stepAcc1.log
+         echo "$totalAcc" >> totalAcc1.log
       fi
-      echo "${avg}" >> time1.log
-      stepAcc=`echo "scale=2; $beforeTime1/$avg" | bc`
-      beforeTime1=$avg
-      totalAcc=`echo "scale=2; $baseline1/$avg" | bc`
-      echo "   stepAcc: $stepAcc, totalAcc: $totalAcc"
-      echo "$stepAcc" >> stepAcc1.log
-      echo "$totalAcc" >> totalAcc1.log
    fi
 done
